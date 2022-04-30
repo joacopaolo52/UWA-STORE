82a39d2c8af40466ccdc5ad7487e157a78747b0b
Joaquin Paolo <joaquinpaolo1@gmail.com>
2022-04-30 19:56:13 -0300

    final 1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 let color = localStorage.getItem("mode");
 const button = document.querySelector('#mode');
+const titulo = document.querySelector('h1');
 
 if(!color)
 {
@@ -10,14 +11,8 @@ if(!color)
 else{
     document.body.style=`background-color:${color}`;
     color === "black" ? button.innerHTML="LIGHT MODE" : button.innerHTML="DARK MODE";
-    // if(color==="black")
-    // {
-    //     button.innerHTML="LIGHT MODE";
-    // }
-    // else{
-    //     button.innerHTML="DARK MODE";
-    // }
 }
+    // if(color==="black") button.innerHTML="LIGHT MODE"; } else{ button.innerHTML="DARK MODE";}
 
 
 button.addEventListener("click", ()=>{
@@ -26,22 +21,32 @@ button.addEventListener("click", ()=>{
     {
         color="white"
         button.innerHTML="DARK MODE";
-        button.setAttribute('style', `background-color: black; color: white;`)
-        for (let i = 1; i <= 18; i++) {
-            let producto = document.querySelector(`.galeria__a${i}`);
-            // producto.setAttribute('style', `box-shadow: 10px 10px 10px 10px rgba(0, 0, 0, 0.7);`);
-        }
+        button.setAttribute('style', `background-color: black; color: white;`);
+        titulo.setAttribute('style', `color: black;`);
     }
     else{
         color="black";
         button.innerHTML="LIGHT MODE";
-        button.setAttribute('style', `background-color: white; color: black;`)
-        for (let i = 1; i <= 18; i++) {
-            let producto = document.querySelector(`.galeria__a${i}`);
-            // producto.setAttribute('style', `box-shadow: 10px 10px 10px 10px rgba(255, 255, 255, .7);`);
-        }
+        button.setAttribute('style', `background-color: white; color: black;`);
+        titulo.setAttribute('style', `color: white;`);
     }
     localStorage.setItem("mode", color);
     document.body.style=`background-color:${color}`;
 
 })
+
+
+    // ERROR DE MEDIA QUERIES //
+
+/*
+
+const head = document.querySelector('head');
+const meta = document.createElement('meta');
+meta.setAttribute('name', 'viewport');
+meta.setAttribute('content', 'width=device-width, initial-scale=1.0');
+
+head.appendChild(meta);
+
+localStorage.setItem('META', meta);
+
+*/
